7e6cc9ebff9801f28e4fa5d99e744f73b18729d8
Pranav Aggarwal <pranavaggarwal321@gmail.com>
2022-05-02 09:30:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    }
+);
